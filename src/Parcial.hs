93bbc2e5fedb58a226f172c36efe0514fd890a17
c4tek3nt <kentcaterina@gmail.com>
2026-05-04 11:41:11 -0300

    correciones

--- src/Parcial.hs
+++ src/Parcial.hs
@@ -1,7 +1,7 @@
 module Parcial where
 import Text.Show.Functions()
 
-data Perrito = Perrito {
+data Perrito = unPerro {
     raza :: String,
     jueguetesFavs :: [String],
     tiempoEnGuarderiaEnMinutos :: Int,
@@ -9,36 +9,35 @@ data Perrito = Perrito {
 } deriving Show
 
 type Guarderia = ([Actividad], TiempoEnMinutos)
-type Actividad = Perritos -> Perritos
+type Actividad = Perrito -> Perrito
 type TiempoEnMinutos = Int
 
 --Modelos
 zara :: Perrito
-zara = Perrito "Dalmata" ["Pelota", "Mantita"] 90 80
+zara = unPerro "Dalmata" ["Pelota", "Mantita"] 90 80
 
 guarderiaPdePerritos :: Guarderia
 guarderiaPdePerritos = ([jugar, ladrar 18, regalar "Pelota", diaDeSpa, diaDeCampo], 30 + 20 + 0 + 120 + 720)
 
 --Funciones
 jugar :: Actividad
-jugar (unPerro, raza, juguetes, tiempo, energia) = unPerro raza juguetes tiempo (max 0(energia - 10))
+jugar (unPerro raza juguetes tiempo energia) = unPerro raza juguetes tiempo (max 0 (energia - 10))
 
-ladrar :: Int -> Actividad
-ladrar cantLadridos (unPerro, raza, juguetes, tiempo, energia) = unPerro raza juguetes tiempo (energia + (cantLadridos/2))
+ladrar :: Number -> Actividad
+ladrar cantLadridos (unPerro raza juguetes tiempo energia) = unPerro raza juguetes tiempo (energia + (cantLadridos/2))
 
 regalar :: String -> Actividad
-regalar nuevoJuguete (unPerro, raza, juguetes, tiempo, energia) = nuevoJuguete : juguetes
+regalar nuevoJuguete (unPerro raza juguetes tiempo energia) = unPerro raza (nuevoJuguete : juguetes) tiempo energia
 
 diaDeSpa :: Actividad 
-diaDeSpa (unPerro, raza, juguetes, tiempo, energia) 
-    | tiempo >= 50 || razaExtravagante raza = energia == 100 && regalar "peineDeGoma"
-    | otherwise unPerro raza juguetes tiempo energia
+diaDeSpa (unPerro raza juguetes tiempo energia) 
+    | tiempo >= 50 || razaExtravagante raza = regalar "peineDeGoma" (unPerro raza juguetes tiempo 100)
 
 razaExtravagante :: String -> Bool
 razaExtravagante raza = raza == "Dalmata" || raza == "Pomerania"
 
 diaDeCampo :: Actividad
-diaDeCampo  (unPerro, raza, juguetes, tiempo, energia) = unPerro raza (perderprimerJuguete juguetes) tiempo energia
+diaDeCampo  (unPerro raza juguetes tiempo energia) = unPerro raza (perderPrimerJuguete juguetes) tiempo energia
 
 perderPrimerJuguete :: [String] -> [String]
 perderPrimerJuguete [] = []
