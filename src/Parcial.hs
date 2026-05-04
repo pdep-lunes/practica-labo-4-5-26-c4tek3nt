97d7f8bc228342abe8a63400ea375815a5f007aa
facundoHarche1 <facundoHarche1@gmail.com>
2026-05-04 11:05:39 -0300

    modelado de datos y funciones

--- src/Parcial.hs
+++ src/Parcial.hs
@@ -1,5 +1,45 @@
 module Parcial where
 import Text.Show.Functions()
 
-doble :: Int -> Int
-doble = (*2)
+data Perrito = Perrito {
+    raza :: String,
+    jueguetesFavs :: [String],
+    tiempoEnGuarderiaEnMinutos :: Int,
+    energia :: Int
+} deriving Show
+
+type Guarderia = ([Actividad], TiempoEnMinutos)
+type Actividad = Perritos -> Perritos
+type TiempoEnMinutos = Int
+
+--Modelos
+zara :: Perrito
+zara = Perrito "Dalmata" ["Pelota", "Mantita"] 90 80
+
+guarderiaPdePerritos :: Guarderia
+guarderiaPdePerritos = ([jugar, ladrar 18, regalar "Pelota", diaDeSpa, diaDeCampo], 30 + 20 + 0 + 120 + 720)
+
+--Funciones
+jugar :: Actividad
+jugar (unPerro, raza, juguetes, tiempo, energia) = unPerro raza juguetes tiempo (max 0(energia - 10))
+
+ladrar :: Int -> Actividad
+ladrar cantLadridos (unPerro, raza, juguetes, tiempo, energia) = unPerro raza juguetes tiempo (energia + (cantLadridos/2))
+
+regalar :: String -> Actividad
+regalar nuevoJuguete (unPerro, raza, juguetes, tiempo, energia) = nuevoJuguete : juguetes
+
+diaDeSpa :: Actividad 
+diaDeSpa (unPerro, raza, juguetes, tiempo, energia) 
+    | tiempo >= 50 || razaExtravagante raza = energia == 100 && regalar "peineDeGoma"
+    | otherwise unPerro raza juguetes tiempo energia
+
+razaExtravagante :: String -> Bool
+razaExtravagante raza = raza == "Dalmata" || raza == "Pomerania"
+
+diaDeCampo :: Actividad
+diaDeCampo  (unPerro, raza, juguetes, tiempo, energia) = unPerro raza (perderprimerJuguete juguetes) tiempo energia
+
+perderPrimerJuguete :: [String] -> [String]
+perderPrimerJuguete [] = []
+perderPrimerJuguete (_ : resto) = resto
